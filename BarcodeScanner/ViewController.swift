65c62670bb6033ce029a87f066c022047d5f3e0d
Ross Bower <ross@ross-mbp.wireless.dartmouth.edu>
2017-09-25 17:44:03 -0400

    added initial json, need to still parse and display item name

--- BarcodeScanner/ViewController.swift
+++ BarcodeScanner/ViewController.swift
@@ -7,12 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {     var cameraView: CameraView!
+    
+    let session = AVCaptureSession()
+    let sessionQueue = DispatchQueue(label: AVCaptureSession.self.description(), attributes: [], target: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        session.beginConfiguration()
+        
+        let videoDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        if (videoDevice != nil) {
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice)
+            
+            if (videoDeviceInput != nil) {
+                if (session.canAddInput(videoDeviceInput)) {
+                    session.addInput(videoDeviceInput)
+                }
+            }
+            
+            let metadataOutput = AVCaptureMetadataOutput()
+            
+            if (session.canAddOutput(metadataOutput)) {
+                session.addOutput(metadataOutput)
+                
+                metadataOutput.metadataObjectTypes = [
+                    AVMetadataObjectTypeEAN13Code,
+                    AVMetadataObjectTypeQRCode
+                ]
+                
+                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            }
+        }
+        
+        session.commitConfiguration()
+        
+        cameraView.layer.session = session
+        cameraView.layer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        let videoOrientation: AVCaptureVideoOrientation
+        switch UIApplication.shared.statusBarOrientation {
+        case .portrait:
+            videoOrientation = .portrait
+            
+        case .portraitUpsideDown:
+            videoOrientation = .portraitUpsideDown
+            
+        case .landscapeLeft:
+            videoOrientation = .landscapeLeft
+            
+        case .landscapeRight:
+            videoOrientation = .landscapeRight
+            
+        default:
+            videoOrientation = .portrait
+        }
+        
+        cameraView.layer.connection.videoOrientation = videoOrientation
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +75,95 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func loadView() {
+        cameraView = CameraView()
+        
+        view = cameraView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        sessionQueue.async {
+            self.session.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sessionQueue.async {
+            self.session.stopRunning()
+        }
+    }
 
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        // Update camera orientation
+        let videoOrientation: AVCaptureVideoOrientation
+        switch UIDevice.current.orientation {
+        case .portrait:
+            videoOrientation = .portrait
+            
+        case .portraitUpsideDown:
+            videoOrientation = .portraitUpsideDown
+            
+        case .landscapeLeft:
+            videoOrientation = .landscapeRight
+            
+        case .landscapeRight:
+            videoOrientation = .landscapeLeft
+            
+        default:
+            videoOrientation = .portrait
+        }
+        
+        cameraView.layer.connection.videoOrientation = videoOrientation
+    }
+
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        if (metadataObjects.count > 0 && metadataObjects.first is AVMetadataMachineReadableCodeObject) {
+            let scan = metadataObjects.first as! AVMetadataMachineReadableCodeObject
+            let barcode_number = scan.stringValue
+
+            let urlString = URL(string: "http://api.upcdatabase.org/json/a3d445a016d7c34a61b83ff1594ab361/0111222333446") //need to put our barcode number in the last field
+            if let url = urlString {
+                let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                    if error != nil {
+                        print(error)
+                    } else {
+                        if let usableData = data {
+                            //print(usableData)
+                            
+                            let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+
+                            print("THIS IS JSON", json) //Prints json to console
+                        }
+                    }
+                }
+                task.resume()
+                
+            // need to extract the item name and display it in message
+            let alertController = UIAlertController(title: "Barcode Scanned", message: barcode_number, preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler:nil))
+            
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    }}
+
+class CameraView: UIView {
+    override class var layerClass: AnyClass {
+        get {
+            return AVCaptureVideoPreviewLayer.self
+        }
+    }
+    
+    override var layer: AVCaptureVideoPreviewLayer {
+        get {
+            return super.layer as! AVCaptureVideoPreviewLayer
+        }
+    }
 }
-
